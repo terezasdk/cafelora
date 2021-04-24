074c39f5abf8b5ffdc07a3a6c5d747de462e2a4c
Tereza Sudíková <tereza.sudik@gmail.com>
2021-04-24 12:17:09 +0200

    ukol 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 console.log('funguju!');
+
+document.querySelector('#nav-btn').addEventListener('click', (event) => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+document.querySelectorAll('nav').forEach((navlink) => {
+  navlink.addEventListener('click', () => {
+    navlink.classList.toggle('nav-closed');
+  });
+});
+
+// připojte posluchač události také na všechny položky navigace. Zařiďte, aby se navigace při kliknutí na libovolnou její položku schovala. Zde se vám jistě bude hodit metoda querySelectorAll.
