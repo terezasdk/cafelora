2d89187a0742af2a96ff6396d9d3a6d0d531e8b1
Tereza Sudíková <tereza.sudik@gmail.com>
2021-04-25 12:22:46 +0200

    ukol 7 napoj jako komponenta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import { Layer } from './Layer/index'; // z úkolu 5
+import { Drink } from './Drink/index'; // z úkolu 7
 
 console.log('funguju!');
 
@@ -15,59 +16,79 @@ document.querySelectorAll('nav').forEach((navlink) => {
 });
 
 // 4 Objednávání
-const orderBtnElm = document.querySelector('.order-btn');
-const drinkCupElm = document.querySelector('.drink__cup');
-let ordered = false;
+// const orderBtnElm = document.querySelector('.order-btn');
+// const drinkCupElm = document.querySelector('.drink__cup');
+// let ordered = false;
 
-orderBtnElm.addEventListener('click', () => {
-  if (ordered === false) {
-    drinkCupElm.classList.add('drink__cup--selected');
-    orderBtnElm.textContent = 'Zrušit';
-    ordered = true;
-  } else {
-    drinkCupElm.classList.remove('drink__cup--selected');
-    orderBtnElm.textContent = 'Objednat';
-    ordered = false;
-  }
-});
+// orderBtnElm.addEventListener('click', () => {
+//   if (ordered === false) {
+//     drinkCupElm.classList.add('drink__cup--selected');
+//     orderBtnElm.textContent = 'Zrušit';
+//     ordered = true;
+//   } else {
+//     drinkCupElm.classList.remove('drink__cup--selected');
+//     orderBtnElm.textContent = 'Objednat';
+//     ordered = false;
+//   }
+// });
 
-// 5 Ingredience jako komponenty
-const item1 = {
-  color: '#feeeca',
-  label: 'mléčná pěna',
-};
+// // 5 Ingredience jako komponenty
+// const item1 = {
+//   color: '#feeeca',
+//   label: 'mléčná pěna',
+// };
 
-const item2 = {
-  color: '#fed7b0',
-  label: 'teplé mléko',
-};
+// const item2 = {
+//   color: '#fed7b0',
+//   label: 'teplé mléko',
+// };
 
-const item3 = {
-  color: '#613916',
-  label: 'espresso',
-};
+// const item3 = {
+//   color: '#613916',
+//   label: 'espresso',
+// };
 
-const drinkInfoElm = document.querySelector('.drink__info');
-//drinkInfoElm.innerHTML += Layer(item1);
-//drinkInfoElm.innerHTML += Layer(item2);
-//drinkInfoElm.innerHTML += Layer(item3);
+// const drinkInfoElm = document.querySelector('.drink__info');
+// //drinkInfoElm.innerHTML += Layer(item1);
+// //drinkInfoElm.innerHTML += Layer(item2);
+// //drinkInfoElm.innerHTML += Layer(item3);
 
-// 6 Seznam ingrediencí (dělá to to samé jako 5, jen je to jiný kód)
-const layers = [
-  {
-    color: '#feeeca',
-    label: 'mléčná pěna',
-  },
-  {
-    color: '#fed7b0',
-    label: 'teplé mléko',
-  },
-  {
-    color: '#613916',
-    label: 'espresso',
-  },
-];
+// // 6 Seznam ingrediencí (dělá to to samé jako 5, jen je to jiný kód)
+// const layers = [
+//   {
+//     color: '#feeeca',
+//     label: 'mléčná pěna',
+//   },
+//   {
+//     color: '#fed7b0',
+//     label: 'teplé mléko',
+//   },
+//   {
+//     color: '#613916',
+//     label: 'espresso',
+//   },
+// ];
 
-layers.forEach((item) => {
-  drinkInfoElm.innerHTML += Layer(item); // const drinkInfoElm definuju na řádku 50
-});
+// layers.forEach((item) => {
+//   drinkInfoElm.innerHTML += Layer(item); // const drinkInfoElm definuju na řádku 50
+// });
+
+// 7 Nápoj jako komponenta
+const drinksList = document.querySelector('.drinks-list');
+drinksList.append(
+  Drink({
+    id: 'romano',
+    name: 'Romano',
+    ordered: false,
+    layers: [
+      {
+        color: '#fbdf5b',
+        label: 'citrón',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+  }),
+);
